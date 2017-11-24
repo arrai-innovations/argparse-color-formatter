dcc85c06505c7b5e640bf6c53bf766d7f4e6b2ec
Joel Hillacre <joel@403forbidden.ca>
2017-11-24 14:27:03 -0700

    make setup.py actually distribute the source.

--- setup.py
+++ setup.py
@@ -1,22 +1,19 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
 setup(
     name='argparse_color_formatter',
     url='https://github.com/emergence/argparse-color-formatter/',
     version='1.0.0',
-    description='a `formatter_class` for argparse that knows how to deal with color escapes.',
+    description='a `formatter_class` for argparse that knows how to deal with ansi color escapes.',
     author='Emergence by Design',
     author_email='support@emergence.com',
-    packages=find_packages(),
+    py_modules=['argparse_color_formatter'],
     install_requires=[x for x in open('requirements.txt').read().split('\n') if x],
-    include_package_data=True,
-    zip_safe=False,
     classifiers=[
         'Development Status :: 4 - Beta',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'License :: OSI Approved :: BSD-3-Clause',
-        'Environment :: Console',
-        'Environment :: Console',
+        'Environment :: Console'
     ]
 )
