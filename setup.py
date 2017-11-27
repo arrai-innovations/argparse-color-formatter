51b897fa929d61b0389a184a82a3a3a19798032a
Joel Hillacre <joel@403forbidden.ca>
2017-11-27 12:54:01 -0700

    add long description for pipy page, fix description to be rst.

--- setup.py
+++ setup.py
@@ -4,9 +4,31 @@ setup(
     name='argparse-color-formatter',
     url='https://github.com/emergence/argparse-color-formatter/',
     version='1.0.0',
-    description='a `formatter_class` for `argparse` that deals with ANSI colour escapes.'
+    description='a ``formatter_class`` for ``argparse`` that deals with ANSI colour escapes.'
                 ' Specifically, this formatter does not count escape characters as displayed'
-                ' characters when wrapping `argparse`\'s help text into the terminal.',
+                ' characters when wrapping ``argparse``\'s help text into the terminal.',
+    long_description='''
+Install
+-------
+
+.. code-block:: shell
+
+   $ pip install argparse-color-formatter
+
+Usage
+-----
+
+Pass in ``argparse_color_formatter.ColorHelpFormatter`` to a new argument parser as ``formatter_class``
+
+.. code-block:: python
+
+   import argparse
+   from argparse_color_formatter import ColorHelpFormatter
+
+   parser = argparse.ArgumentParser(
+       formatter_class=ColorHelpFormatter
+   )
+    ''',
     author='Emergence by Design',
     author_email='support@emergence.com',
     py_modules=['argparse_color_formatter'],
